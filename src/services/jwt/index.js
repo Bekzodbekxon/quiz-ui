f7272713f4ea272a23ea136520beedf01f9302da
Bekzodbekxon <qweasd123>
2021-01-19 22:57:54 +0500

    first commit

---
+++ src/services/jwt/index.js
@@ -0,0 +1,70 @@
+import apiClient from '@/services/axios'
+import store from 'store'
+
+export async function login(username, password) {
+  debugger
+  return apiClient
+    .post('auth/authentication', {
+      username,
+      password,
+    })
+    .then(response => {
+      if (response) {
+        debugger
+        const { token } = response.data
+        if (token) {
+          store.set('accessToken', token)
+        }
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function register(email, password, name) {
+  return apiClient
+    .post('/auth/register', {
+      email,
+      password,
+      name,
+    })
+    .then(response => {
+      if (response) {
+        const { accessToken } = response.data
+        if (accessToken) {
+          store.set('accessToken', accessToken)
+        }
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function currentAccount() {
+  debugger
+  return apiClient
+    .get('/auth/account')
+    .then(response => {
+      if (response) {
+        const { accessToken } = response.data
+        if (accessToken) {
+          store.set('accessToken', accessToken)
+        }
+        return response.data
+      }
+      return false
+    })
+    .catch(err => console.log(err))
+}
+
+export async function logout() {
+  return apiClient
+    .get('/auth/logout')
+    .then(() => {
+      store.remove('accessToken')
+      return true
+    })
+    .catch(err => console.log(err))
+}
